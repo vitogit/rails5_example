a5c728444c5e0b60746fa125c604da0b6b0160ad
vitogit <vitogit@users.noreply.github.com>
2019-04-11 14:50:50 -0300

    Create balance API with specs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   def payment
-
     payment = Payment.new(amount: valid_params[:amount], 
                    sender_id: valid_params[:id], 
                    receiver_id: valid_params[:friend_id], 
@@ -13,13 +12,22 @@ class UsersController < ApplicationController
 
   end
 
+  def balance
+    user = User.find_by(id: valid_params[:id])
+    if user && user.payment_account
+      json_response({ amount: user.payment_account.balance })
+    else
+      json_response({ message: 'Invalid params' }, 400)
+    end
+  end
+
   private
 
   def valid_params
     # whitelist params
     params.permit(:id, :friend_id, :amount, :description)
   end
-  
+
   def json_response(object, status = :ok)
     render json: object, status: status
   end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe "Users API", type: :request do
     context "with invalid friend" do
       before { post "/user/#{user_1.id}/payment", params: { friend_id: user_2.id+10, amount: 100, description: "Yeah" } }
 
-      it "returns status code 404" do
+      it "returns status code 400" do
         expect(response).to have_http_status(400)
       end
 
@@ -36,6 +36,32 @@ RSpec.describe "Users API", type: :request do
 
       it "returns a invalid range number for amount" do
         expect(response.body).to match(/must be less than 1000/)
+      end
+    end
+  end
+
+  describe "Get /user/{id}/balance" do
+    let!(:payment_account) { create(:payment_account, user_id: user_1.id, balance: 1000) }
+
+    context "with valid params" do
+      before { post "/user/#{user_1.id}/balance"}
+      it "returns status code 200 with empty body" do
+        expect(response).to have_http_status(200)
+      end
+      it "returns the correct amount" do
+        expect(response.body).to match(/1000.0/)
+      end
+    end
+
+    context "with invalid user" do
+      before { post "/user/#{user_1.id+10}/balance" }
+
+      it "returns status code 400" do
+        expect(response).to have_http_status(400)
+      end
+
+      it "returns a error message" do
+        expect(response.body).to match(/Invalid params/)
       end
     end
   end
