2c214fa8fe53988202adf5afc9daaeae8782ffdf
vitogit <vitogit@users.noreply.github.com>
2019-04-11 11:53:26 -0300

    Fix payment model syntax

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,5 +1,6 @@
 class Payment < ApplicationRecord
-  belongs_to :sender, class_name: "User", primary_key: "sender_id"
-  belongs_to :receiver, class_name: "User", primary_key: "receiver_id"
+  belongs_to :sender, foreign_key: 'sender_id', class_name: 'User'
+  belongs_to :receiver, foreign_key: 'receiver_id', class_name: 'User'
   validates :amount, numericality: { less_than: 1000, greater_than: 0 }
 end
+
